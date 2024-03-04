2e37fb984494641e3ec19491adb2d51d2c115ead
dasom <dasom589@gmail.com>
2024-03-04 18:01:13 +0900

    2024-03-04

---
+++ src/main/java/com/javaex/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	//메소드 - 일반
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+
+	public String hello() {
+
+		System.out.println("/hellospring/hello");
+
+		return "/WEB-INF/views/index.jsp";
+
+	}
+
+}
